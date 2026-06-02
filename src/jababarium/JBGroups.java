839440d295f7ebc9deb6f29cfca00512dede67a9
kadi4ok <thunderrobot29@gmail.com>
2026-06-02 14:13:05 +0300

    sprite

--- src/jababarium/JBGroups.java
+++ src/jababarium/JBGroups.java
@@ -12,7 +12,6 @@ import static mindustry.Vars.world;
 
 public class JBGroups {
 
-    //gfdgdfdf
     protected static final Seq<GravityTrapField> tmpGravityFields = new Seq<>();
     protected static final Rect tmpRect = new Rect();
     public static QuadTree<GravityTrapField> gravityFields = new QuadTree<>(new Rect());
